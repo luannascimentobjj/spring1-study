e8a212b1ea480bb1afc266b2db028a38495e0568
luannascimentobjj <nascimento.hate@gmail.com>
2017-08-19 18:38:04 -0300

    inclusão do produto validation, massage propertis, o form do spring e também o mvcUrl

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,14 +17,19 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validations.ProdutoValidation;
 
 @Controller
-@RequestMapping("produtos")
+@RequestMapping("/produtos")
 public class ProdutosController {
 
 	@Autowired
 	private ProdutoDAO produtodDao;
 	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
 	
 	@RequestMapping("/form")
 	public ModelAndView form() {
@@ -29,8 +39,11 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView grava(Produto produto, RedirectAttributes redirectAttributes) {
-		System.out.println(produto);
+	public ModelAndView grava(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
+		
+		if(result.hasErrors()) {
+			return form();
+		}
 		produtodDao.gravar(produto);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
 		return new ModelAndView("redirect:produtos");
