6773b9aa804c72c1e79610baf41225764ca2c2df
luannascimentobjj <nascimento.hate@gmail.com>
2017-08-19 11:53:13 -0300

    inclusão do jpaconfiguration, hibernate e primeira inserçao no banco

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -0,0 +1,23 @@
+package br.com.casadocodigo.loja.daos;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+@Repository
+@Transactional
+public class ProdutoDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	
+	public void gravar(Produto produto) {
+		em.persist(produto);
+	}
+	
+}
