58241136c5b83f85bb754551259c25a4be118b84
luannascimentobjj <nascimento.hate@gmail.com>
2017-08-28 22:26:26 -0300

    inclusão do carrinho de compras, total e valor

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,13 +28,17 @@ public class CarrinhoComprasController {
 	
 	@RequestMapping("/add")
 	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
-		ModelAndView mv = new ModelAndView("redirect:/produtos");
+		ModelAndView mv = new ModelAndView("redirect:/carrinho");
 		CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
 		carrinho.add(carrinhoItem);
 		return mv;
 		
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView itens() {
+		return new ModelAndView("/carrinho/itens");
+	}
 	
 	private CarrinhoItem criaItem(Integer produtoId, TipoPreco tipoPreco) {
 		Produto produto = produtoDAO.find(produtoId);
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,7 +12,9 @@ import org.springframework.web.context.WebApplicationContext;
 
 @Component
 @Scope(value=WebApplicationContext.SCOPE_SESSION)
-public class CarrinhoCompras {
+public class CarrinhoCompras implements Serializable{
+	
+	private static final long serialVersionUID = -2106272778536854933L;
 	
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
 	
@@ -17,7 +22,7 @@ public class CarrinhoCompras {
 		itens.put(item, getQuantidade(item) + 1);
 	}
 
-	private int getQuantidade(CarrinhoItem item) {
+	public Integer getQuantidade(CarrinhoItem item) {
 		if(!itens.containsKey(item)) {
 			itens.put(item, 0);
 		}
@@ -27,5 +32,23 @@ public class CarrinhoCompras {
 	public int getQuantidade() {
 		return itens.values().stream().reduce(0, (proximo, acumulador) -> proximo + acumulador);
 	}
+
+	public Collection<CarrinhoItem> getItens() {
+		return itens.keySet();
+	}
+
+	public BigDecimal getTotal(CarrinhoItem item) {
+		return item.getTotal(getQuantidade(item));
+	}
+	
+	public BigDecimal getTotal() {
+		BigDecimal total = BigDecimal.ZERO;
+		for (CarrinhoItem item : itens.keySet()) {
+			total = total.add(getTotal(item));
+		}
+		return total;
+		
+	}
+
 
 }
